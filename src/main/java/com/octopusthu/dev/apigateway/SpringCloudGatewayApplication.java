7c756b32c66190ef969df0bb2875bf3774e660c3
octopusthu <octopusthu@gmail.com>
2021-12-29 21:35:15 +0800

    updates

---
+++ src/main/java/com/octopusthu/dev/apigateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.octopusthu.dev.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The Spring Cloud Gateway Application
+ *
+ * @author octopusthu
+ */
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+}
